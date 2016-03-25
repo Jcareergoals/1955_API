5741f8fc873d826bff92e57004fe512fee39cd85
Jose Chery <Jcareergoals@hotmail.com>
2016-03-25 17:31:59 -0400

    created the basic routes for the API server application

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser'); 
+var mongoose = require('mongoose'); 
 
 var app = express(); 
 
@@ -11,12 +12,23 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, '/views')); 
 app.set('view engine', 'ejs'); 
 
-var mongoose = require('mongoose'); 
 mongoose.connect('mongodb://localhost/1955_api'); 
 
+
+
+// routes will eventually be delegated to a controller folder
 app.get('/', function(req, res){
-	app.send('You made it this far buddy!'); 
+	res.send('will serve up the full collection of people born in 1955.'); 
 }); 
+app.get('/new/:name', function(req, res){
+	res.send('will add '+req.params.name+' into the database.'); 
+}); 
+app.get('/remove/:name', function(req, res){
+	res.send('will delete '+req.params.name+' from the database.'); 
+}); 
+app.get('/:name', function(req, res){
+	res.send('will bring up the document of that particular person: '+req.params.name); 
+})
 
 app.listen(8000, function(){
 	console.log("Listening at port: 8000"); 
